a242c572112ed93b7550b7d707f173541c074b02
Stullet <lirabusiness7@gmail.com>
2026-02-12 00:12:40 +0000

    mas esse é o outro. o meu é o primeiro que nao esta linkado

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -10,9 +10,9 @@
 // You can find it in your Firebase project settings.
 export const firebaseConfig = {
   apiKey: "mock-api-key",
-  authDomain: "studio-3062338612-27b65.firebaseapp.com",
-  projectId: "studio-3062338612-27b65",
-  storageBucket: "studio-3062338612-27b65.appspot.com",
-  messagingSenderId: "232252924194",
-  appId: "1:232252924194:web:04312ce02ead72f12a8a81"
+  authDomain: "studio-359365564245.firebaseapp.com",
+  projectId: "studio-359365564245",
+  storageBucket: "studio-359365564245.appspot.com",
+  messagingSenderId: "359365564245",
+  appId: "1:359365564245:web:a1b2c3d4e5f6a7b8c9d0e1"
 };
