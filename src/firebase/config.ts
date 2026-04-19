3fa001cca89f7c3ce65455bd39882654766c6d3d
Stullet <lirabusiness7@gmail.com>
2026-04-19 23:38:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "dexban-x-2-28620676-f7550",
+  "appId": "1:1001793351214:web:ab103e52b824f82309f9bf",
+  "apiKey": "AIzaSyCy4HG1NenFhNmXd5C-TYtzVl56EpZZabA",
+  "authDomain": "dexban-x-2-28620676-f7550.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1001793351214"
+};
